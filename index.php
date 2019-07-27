e4e687769b9644fca862e1cf5b3ac1a2a8e843e5
Mohammed Yehia <engmohammedyehia@hotmail.com>
2019-07-28 00:10:21 +0200

    * Created a MockServer Class

--- index.php
+++ index.php
@@ -1,22 +1,9 @@
 <?php
 
-use Swoole\Http\Server;
-use Swoole\Http\Request;
-use Swoole\Http\Response;
+require_once __DIR__.DIRECTORY_SEPARATOR.
+    'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
 
-$server = new Server("0.0.0.0", 9501);
+use App\MockServer\MockServer;
 
-$server->on("start", function ($server) {
-    echo "Swoole http server is started at http://127.0.0.1:9501\n";
-});
-
-$server->on("request", function (Request $request, Response $response) {
-    $response->header("Content-Type", "application/json");
-    $object = new stdClass();
-    $object->name = 'Mohammed Abdoh';
-    $object->age = 37;
-    $body = json_encode($object, JSON_PRETTY_PRINT);
-    $response->end("$body\n");
-});
-
-$server->start();
+$server = new MockServer('127.0.0.1', 9501);
+$server->listen();
--- src/MockServer/MockServer.php
+++ src/MockServer/MockServer.php
@@ -1,19 +1,53 @@
 <?php
+namespace App\MockServer;
 
 use Swoole\Http\Server;
 
+/**
+ * Class MockServer
+ * @package App\MockServer
+ * @author Mohammed Abdoh <engmohammedyehia@hotmail.com>
+ */
 class MockServer
 {
+    /**
+     * @var Server
+     */
     private $server;
-    private $port;
 
+    /**
+     * MockServer constructor.
+     * @param string $ipAddress
+     * @param int $portNumber
+     */
     public function __construct(string $ipAddress, int $portNumber)
     {
-        $this->server = new Server()
+        $this->server = new Server($ipAddress, $portNumber);
+        $this->getServer()->on(
+            'start',
+            function () {
+                printf(
+                    'Server started listening at %s on %d',
+                    $this->getServer()->host,
+                    $this->getServer()->port
+                );
+            }
+        );
     }
 
-    public function start()
+    /**
+     * Listens to requests on the server
+     */
+    public function listen(): void
     {
-        $server = new 
+        $this->getServer()->start();
+    }
+
+    /**
+     * @return Server
+     */
+    public function getServer(): Server
+    {
+        return $this->server;
     }
 }
